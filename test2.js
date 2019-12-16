bf15ae6806ddffdf10c08e41541c4c097cadf87e
Jehyeuk <jhyoo@vetec.co.kr>
2019-12-16 16:15:04 +0900

    test1

---
+++ test2.js
@@ -0,0 +1,12 @@
+console.log('test');
+
+var http = require('http');
+
+http.createServer(function (request, response){
+    response.writeHead(200,{'content-type':'text/html'});
+    response.write('Hello world');
+    response.end();
+
+}).listen(8889);
+
+console.log('server start');
